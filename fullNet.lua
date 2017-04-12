fd2363de9ee0d0dd76d8fa23c1a08c59fd93a9dc
KJoslyn <KJoslyn29@gmail.com>
2017-04-12 18:23:50 -0400

    Reorganized fullNet into functions

--- fullNet.lua
+++ fullNet.lua
@@ -1,247 +1,289 @@
-require 'nn'
-require 'loadcaffe' -- doesn't work on server
-require 'image'
-require 'optim'
-require 'nnlr'
-require 'cutorch'
-require 'cunn'
-require 'cudnn'
 
--- Variable Parameters
-numEpochs = 400 -- 416 is max number without truncating an epoch
--- posExamplesPerEpoch = 1e4
--- negExamplesPerEpoch = 5e4
-posExamplesPerEpoch = 20*100
-negExamplesPerEpoch = 100*100
-L = 8
-k = 4
-hashLayerSize = L * k
-local baseLearningRate = 1e-6
-local baseWeightDecay = 0
-lrMultForHashLayer = 1e5 -- 1e4, 1e5, etc
-posExamplesPerBatch = 20
-negExamplesPerBatch = 100
+-- loadModelAndData()
+-- trainAndEvaluate()
 
--- These are inferred from above
-epochSize = posExamplesPerEpoch + negExamplesPerEpoch
-totNumExamplesPerBatch = posExamplesPerBatch + negExamplesPerBatch
-numBatches = epochSize / totNumExamplesPerBatch
+function loadPackages() 
 
--- Variable Boolean Parameters (1 or 0)
-trainOnOneBatch   = 0
-loadModelFromFile = 0
-saveModelToFile   = 0
+  require 'nn'
+  require 'loadcaffe' -- doesn't work on server
+  require 'image'
+  require 'optim'
+  require 'nnlr'
+  require 'cutorch'
+  require 'cunn'
+  require 'cudnn'
 
--- Fixed Parameters
-I = 1 -- Table index for image modality
-X = 2 -- Table index for text modality
-filePath = '/home/kjoslyn/kevin/' -- server
-snapshotDir = '/home/kjoslyn/kevin/FullNet/snapshots'
+end -- end loadPackages()
 
-package.loaded.pickSubset = nil
-package.loaded.evaluate = nil
-package.loaded.dataLoader = nil
-package.loaded.batchLoader = nil
-package.loaded.createModel = nil
-require 'pickSubset'
-require 'evaluate'
-require 'dataLoader'
-require 'batchLoader'
-require 'createModel'
+function loadModelSnapshot()
 
-if not model then
-    if loadModelFromFile == 1 then
-        print('***Loading model from file')
-        modelData = torch.load('modelData.t7')
-        imageClassifier = modelData.imageClassifier
-        imageHasher = modelData.imageHasher
-        textClassifier = modelData.textClassifier
-        textHasher = modelData.textHasher
-        model = modelData.combinedModel
-    else
-        imageClassifier, imageHasher = getImageModel()
-        textClassifier, textHasher = getTextModel()
-        model = createCombinedModel(imageHasher, textHasher)
+  snapshot2ndLevelDir = 'Lr5e4_5kquery_5kdatabase'
+  snapshotFileName = 'snapshot_epoch_100.t7'
 
-        if saveModelToFile == 1 then
-        print('***Saving model to file')
-        modelData = {}
-        modelData.imageClassifier = imageClassifier
-        modelData.imageHasher = imageHasher
-        modelData.textClassifier = textClassifier
-        modelData.textHasher = textHasher
-        modelData.combinedModel = model
-        torch.save('modelData.t7', modelData)
-        else
-        print('***Skipping save model to file')
-        end
-    end
+  snapshotFullPath = snapshotDir .. '/' .. snapshot2ndLevelDir .. '/' .. snapshotFileName
+  snapshot = torch.load(snapshotFullPath)
+  N = snapshot.params:size(1)
+
+  params, gparams = model:getParameters()
+
+  batchSize = 1e5
+
+  local numBatches = torch.ceil(N / batchSize)
+  for b = 0, numBatches - 1 do
+      startIndex = b * batchSize + 1
+      endIndex = math.min((b + 1) * batchSize, N)
+      params[ {{ startIndex, endIndex }} ]:copy(snapshot.params[ {{ startIndex, endIndex }} ])
+  end
+
 end
 
--- TODO: Make these return something instead of global variables?
-if not trainset then
-    trainset = {}
-    testset = {}
-    getData()
-end
+function loadModelAndData() 
 
-if not pos_pairs then
-    pos_pairs, neg_pairs, p_size, n_size = pickSubset(true)
-end
--- TODO: These are global variables that come from pickSubset. I want to return them instead but the compiler gives an error.
-trainImages = imageIdx:long()
-trainTexts = textIdx:long()
+  if not nn then
+    loadPackages()
+  end
 
--- calcMAP(X, I, nil) -- TODO: Remove
--- calcMAP(I, X, nil) -- TODO: Remove
+  -- Variable Parameters
+  numEpochs = 400 -- 416 is max number without truncating an epoch
+  -- posExamplesPerEpoch = 1e4
+  -- negExamplesPerEpoch = 5e4
+  posExamplesPerEpoch = 20*100
+  negExamplesPerEpoch = 100*100
+  L = 8
+  k = 4
+  hashLayerSize = L * k
+  local baseLearningRate = 1e-6
+  local baseWeightDecay = 0
+  lrMultForHashLayer = 1e5 -- 1e4, 1e5, etc
+  posExamplesPerBatch = 20
+  negExamplesPerBatch = 100
 
-criterion = getCriterion()
+  -- These are inferred from above
+  epochSize = posExamplesPerEpoch + negExamplesPerEpoch
+  totNumExamplesPerBatch = posExamplesPerBatch + negExamplesPerBatch
+  numBatches = epochSize / totNumExamplesPerBatch
 
-learningRates, weightDecays = model:getOptimConfig(baseLearningRate, baseWeightDecay)
+  -- Variable Boolean Parameters (1 or 0)
+  trainOnOneBatch   = 0
+  loadModelFromFile = 0
+  saveModelToFile   = 0
 
-local optimState = {
-      learningRate = baseLearningRate,
-      learningRates = learningRates,
-      weightDecays = weightDecays
-      -- learningRateDecay = 1e-7
-      -- learningRate = 1e-3,
-      -- learningRateDecay = 1e-4
-      -- weightDecay = 0.01,
-      -- momentum = 0.9
-}
+  -- Fixed Parameters
+  I = 1 -- Table index for image modality
+  X = 2 -- Table index for text modality
+  filePath = '/home/kjoslyn/kevin/' -- server
+  snapshotDir = '/home/kjoslyn/kevin/FullNet/snapshots'
 
-params, gradParams = model:getParameters()
+  package.loaded.pickSubset = nil
+  package.loaded.evaluate = nil
+  package.loaded.dataLoader = nil
+  package.loaded.batchLoader = nil
+  package.loaded.createModel = nil
+  require 'pickSubset'
+  require 'evaluate'
+  require 'dataLoader'
+  require 'batchLoader'
+  require 'createModel'
 
--- The label tensor will be the same for each batch
-batch_sim_label = torch.Tensor(posExamplesPerBatch):fill(1)
-batch_sim_label = batch_sim_label:cat(torch.Tensor(negExamplesPerBatch):fill(0))
-batch_sim_label = torch.CudaByteTensor(totNumExamplesPerBatch):copy(batch_sim_label)
-batch_label_for_loss = torch.CudaTensor(totNumExamplesPerBatch):copy(batch_sim_label) * L
+  if not model then
+      if loadModelFromFile == 1 then
+          print('***Loading model from file')
+          modelData = torch.load('modelData.t7')
+          imageClassifier = modelData.imageClassifier
+          imageHasher = modelData.imageHasher
+          textClassifier = modelData.textClassifier
+          textHasher = modelData.textHasher
+          model = modelData.combinedModel
+      else
+          imageClassifier, imageHasher = getImageModel()
+          textClassifier, textHasher = getTextModel()
+          model = createCombinedModel(imageHasher, textHasher)
 
--- -- POS ONLY The label tensor will be the same for each batch
--- batch_sim_label = torch.Tensor(100):fill(1)
--- batch_sim_label = torch.CudaByteTensor(100):copy(batch_sim_label)
--- batch_label_for_loss = torch.CudaTensor(100):copy(batch_sim_label) * L
+          if saveModelToFile == 1 then
+          print('***Saving model to file')
+          modelData = {}
+          modelData.imageClassifier = imageClassifier
+          modelData.imageHasher = imageHasher
+          modelData.textClassifier = textClassifier
+          modelData.textHasher = textHasher
+          modelData.combinedModel = model
+          torch.save('modelData.t7', modelData)
+          else
+          print('***Skipping save model to file')
+          end
+      end
+  end
 
--- -- NEG ONLY The label tensor will be the same for each batch
--- batch_sim_label = torch.Tensor(100):fill(0)
--- batch_sim_label = torch.CudaByteTensor(100):copy(batch_sim_label)
--- batch_label_for_loss = torch.CudaTensor(100):copy(batch_sim_label)
+  -- TODO: Make these return something instead of global variables?
+  if not trainset then
+      trainset = {}
+      testset = {}
+      getData()
+  end
 
-iterationsComplete = 0
+  if not pos_pairs then
+      pos_pairs, neg_pairs, p_size, n_size = pickSubset(true)
+  end
+  -- TODO: These are global variables that come from pickSubset. I want to return them instead but the compiler gives an error.
+  trainImages = imageIdx:long()
+  trainTexts = textIdx:long()
 
-trainBatch = {}
+end -- end loadModelAndData()
 
-if trainOnOneBatch == 1 then
-  print("**************WARNING- Training on one batch only")
-  trainBatch = getBatch(pos_pairs, neg_pairs, p_size, n_size)
-end
+function trainAndEvaluate()
 
-totalLoss = 0
--- epochHistorySize = 5
--- epochHistoryLoss = torch.Tensor(epochHistorySize):fill(0)
+  -- calcMAP(X, I, nil) -- TODO: Remove
+  -- calcMAP(I, X, nil) -- TODO: Remove
 
-date = os.date("*t", os.time())
-dateStr = date.month .. "_" .. date.day .. "_" .. date.hour .. "_" .. date.min
-sf = io.open(snapshotDir .. "/stats_" .. dateStr .. ".txt", "w")
-sfv = io.open(snapshotDir .. "/stats_verbose_" .. dateStr .. ".txt", "w")
+  criterion = getCriterion()
 
-model:training()
+  learningRates, weightDecays = model:getOptimConfig(baseLearningRate, baseWeightDecay)
 
-for epoch = 1, numEpochs  do
+  local optimState = {
+        learningRate = baseLearningRate,
+        learningRates = learningRates,
+        weightDecays = weightDecays
+        -- learningRateDecay = 1e-7
+        -- learningRate = 1e-3,
+        -- learningRateDecay = 1e-4
+        -- weightDecay = 0.01,
+        -- momentum = 0.9
+  }
 
-    epochLoss = 0
+  params, gradParams = model:getParameters()
 
-    for batchNum = 0, numBatches - 1 do
+  -- The label tensor will be the same for each batch
+  batch_sim_label = torch.Tensor(posExamplesPerBatch):fill(1)
+  batch_sim_label = batch_sim_label:cat(torch.Tensor(negExamplesPerBatch):fill(0))
+  batch_sim_label = torch.CudaByteTensor(totNumExamplesPerBatch):copy(batch_sim_label)
+  batch_label_for_loss = torch.CudaTensor(totNumExamplesPerBatch):copy(batch_sim_label) * L
 
-        if trainOnOneBatch == 0 then
-          trainBatch = getBatch(pos_pairs, neg_pairs, p_size, n_size)
-        end
+  -- -- POS ONLY The label tensor will be the same for each batch
+  -- batch_sim_label = torch.Tensor(100):fill(1)
+  -- batch_sim_label = torch.CudaByteTensor(100):copy(batch_sim_label)
+  -- batch_label_for_loss = torch.CudaTensor(100):copy(batch_sim_label) * L
 
-        function feval(x)
-            -- get new parameters
-            if x ~= params then
-               params:copy(x)
-            end         
+  -- -- NEG ONLY The label tensor will be the same for each batch
+  -- batch_sim_label = torch.Tensor(100):fill(0)
+  -- batch_sim_label = torch.CudaByteTensor(100):copy(batch_sim_label)
+  -- batch_label_for_loss = torch.CudaTensor(100):copy(batch_sim_label)
 
-            input = trainBatch.data
-            target = batch_label_for_loss
-            inputSize = input[1]:size(1)
+  iterationsComplete = 0
 
-            gradParams:zero()
+  trainBatch = {}
 
-            output = model:forward(input)
-            local loss = criterion:forward(output, target)
-            local dloss_doutput = criterion:backward(output, target)
-            model:backward(input, dloss_doutput)
+  if trainOnOneBatch == 1 then
+    print("**************WARNING- Training on one batch only")
+    trainBatch = getBatch(pos_pairs, neg_pairs, p_size, n_size)
+  end
 
-            gradParams:div(inputSize)
-            loss = loss/inputSize
+  totalLoss = 0
+  -- epochHistorySize = 5
+  -- epochHistoryLoss = torch.Tensor(epochHistorySize):fill(0)
 
-            -- Stats
-            epochLoss = epochLoss + loss
-            -- epochHistoryLoss[(epoch % epochHistorySize) + 1] = loss
-            totalLoss = totalLoss + loss
+  date = os.date("*t", os.time())
+  dateStr = date.month .. "_" .. date.day .. "_" .. date.hour .. "_" .. date.min
+  sf = io.open(snapshotDir .. "/stats_" .. dateStr .. ".txt", "w")
+  sfv = io.open(snapshotDir .. "/stats_verbose_" .. dateStr .. ".txt", "w")
 
-            return loss, gradParams
-        end
-        optim.sgd(feval, params, optimState)
+  model:training()
 
-        iterationsComplete = iterationsComplete + 1
+  for epoch = 1, numEpochs  do
 
-        -- collectgarbage()
-    end
+      epochLoss = 0
 
-    model:evaluate()
-    -- epochPred = model:forward(trainBatch.data)
+      for batchNum = 0, numBatches - 1 do
 
-    -- batchTextToImageMAP = calcMAP(X, I, trainBatch)
-    -- batchImageToTextMAP = calcMAP(I, X, trainBatch)
-    if epoch % 5 == 0 then
-        textToImageMAP = calcMAP(X, I, nil)
-        imageToTextMAP = calcMAP(I, X, nil)
+          if trainOnOneBatch == 0 then
+            trainBatch = getBatch(pos_pairs, neg_pairs, p_size, n_size)
+          end
 
-        imageAccuracy = calcClassAccuracy(I)
-        textAccuracy = calcClassAccuracy(X)
+          function feval(x)
+              -- get new parameters
+              if x ~= params then
+                params:copy(x)
+              end         
 
-        batchTextClassAcc = calcBatchClassAccuracy(textClassifier, trainBatch, X)
-        batchImageClassAcc = calcBatchClassAccuracy(imageClassifier, trainBatch, I)-- TODO: This is not very useful because it is only for the last batch in the epoch
+              input = trainBatch.data
+              target = batch_label_for_loss
+              inputSize = input[1]:size(1)
 
-        statsPrint("=====Epoch " .. epoch, sf, sfv)
-        calcAndPrintHammingAccuracy(trainBatch, batch_sim_label, sfv) -- TODO: This is not very useful because it is only for the last batch in the epoch
+              gradParams:zero()
 
-        statsPrint(string.format("Avg Loss this epoch = %.2f", epochLoss / numBatches), sf, sfv)
-        statsPrint(string.format("Avg Loss overall = %.2f", totalLoss / iterationsComplete), sf, sfv)
+              output = model:forward(input)
+              local loss = criterion:forward(output, target)
+              local dloss_doutput = criterion:backward(output, target)
+              model:backward(input, dloss_doutput)
 
-        -- statsPrint(string.format("Batch Text to Image MAP = %.2f", batchTextToImageMAP))
-        -- statsPrint(string.format("Batch Image to Text MAP = %.2f", batchImageToTextMAP))
-        statsPrint(string.format("Text to Image MAP = %.2f", textToImageMAP), sf, sfv)
-        statsPrint(string.format("Image to Text MAP = %.2f", imageToTextMAP), sf, sfv)
+              gradParams:div(inputSize)
+              loss = loss/inputSize
 
-        statsPrint(string.format('Image Classification Acc: %.2f', imageAccuracy), sf, sfv)
-        statsPrint(string.format('Text Classification Acc: %.2f', textAccuracy), sf, sfv)
+              -- Stats
+              epochLoss = epochLoss + loss
+              -- epochHistoryLoss[(epoch % epochHistorySize) + 1] = loss
+              totalLoss = totalLoss + loss
 
-        statsPrint(string.format("Batch Text Classification Acc = %.2f", batchTextClassAcc), sfv)
-        statsPrint(string.format("Batch Image Classification Acc = %.2f", batchImageClassAcc), sfv)
-    end
+              return loss, gradParams
+          end
+          optim.sgd(feval, params, optimState)
 
-    if epoch % 50 == 0 then
-        local paramsToSave, gp = model:getParameters()
-        local snapshotFile = snapshotDir .. "/snapshot_epoch_" .. epoch .. ".t7" 
-        local snapshot = {}
-        snapshot.params = paramsToSave
-        if epoch % 100 == 0 then
-            snapshot.gparams = gp
-        end
-        torch.save(snapshotFile, snapshot)
-    end
+          iterationsComplete = iterationsComplete + 1
 
-    model:training()
-end
+          -- collectgarbage()
+      end
 
-io.close(sf)
-io.close(sfv)
+      model:evaluate()
+      -- epochPred = model:forward(trainBatch.data)
 
---[[
-model:evaluate()
---]]
+      -- batchTextToImageMAP = calcMAP(X, I, trainBatch)
+      -- batchImageToTextMAP = calcMAP(I, X, trainBatch)
+      if epoch % 5 == 0 then
+          textToImageMAP = calcMAP(X, I, nil)
+          imageToTextMAP = calcMAP(I, X, nil)
+
+          imageAccuracy = calcClassAccuracy(I)
+          textAccuracy = calcClassAccuracy(X)
+
+          batchTextClassAcc = calcBatchClassAccuracy(textClassifier, trainBatch, X)
+          batchImageClassAcc = calcBatchClassAccuracy(imageClassifier, trainBatch, I)-- TODO: This is not very useful because it is only for the last batch in the epoch
+
+          statsPrint("=====Epoch " .. epoch, sf, sfv)
+          calcAndPrintHammingAccuracy(trainBatch, batch_sim_label, sfv) -- TODO: This is not very useful because it is only for the last batch in the epoch
+
+          statsPrint(string.format("Avg Loss this epoch = %.2f", epochLoss / numBatches), sf, sfv)
+          statsPrint(string.format("Avg Loss overall = %.2f", totalLoss / iterationsComplete), sf, sfv)
+
+          -- statsPrint(string.format("Batch Text to Image MAP = %.2f", batchTextToImageMAP))
+          -- statsPrint(string.format("Batch Image to Text MAP = %.2f", batchImageToTextMAP))
+          statsPrint(string.format("Text to Image MAP = %.2f", textToImageMAP), sf, sfv)
+          statsPrint(string.format("Image to Text MAP = %.2f", imageToTextMAP), sf, sfv)
+
+          statsPrint(string.format('Image Classification Acc: %.2f', imageAccuracy), sf, sfv)
+          statsPrint(string.format('Text Classification Acc: %.2f', textAccuracy), sf, sfv)
+
+          statsPrint(string.format("Batch Text Classification Acc = %.2f", batchTextClassAcc), sfv)
+          statsPrint(string.format("Batch Image Classification Acc = %.2f", batchImageClassAcc), sfv)
+      end
+
+      if epoch % 50 == 0 then
+          local paramsToSave, gp = model:getParameters()
+          local snapshotFile = snapshotDir .. "/snapshot_epoch_" .. epoch .. ".t7" 
+          local snapshot = {}
+          snapshot.params = paramsToSave
+          if epoch % 100 == 0 then
+              snapshot.gparams = gp
+          end
+          torch.save(snapshotFile, snapshot)
+      end
+
+      model:training()
+  end
+
+  io.close(sf)
+  io.close(sfv)
+
+  --[[
+  model:evaluate()
+  --]]
+
+end -- end trainAndEvaluate()
